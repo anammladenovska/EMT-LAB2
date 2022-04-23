ddfb266c6f3c098955c51bbe7822ed62b5770187
anammladenovska <80364724+anammladenovska@users.noreply.github.com>
2022-04-23 12:34:58 +0200

    Add files via upload

---
+++ lab2/src/main/java/mk/ukim/finki/emt/lab2/config/CustomUsernamePasswordAuthenticationProvider.java
@@ -0,0 +1,21 @@
+package mk.ukim.finki.emt.lab2.config;
+
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomUsernamePasswordAuthenticationProvider implements AuthenticationProvider {
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        return null;
+    }
+
+    @Override
+    public boolean supports(Class<?> aClass) {
+        return aClass.equals(UsernamePasswordAuthenticationToken.class);
+    }
+}
